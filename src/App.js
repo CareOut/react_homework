bea486bd4b245c9149f2ebe5ef4666ae3b7d651e
Stanislav <9226303141@mail.ru>
2022-08-22 20:37:25 +0500

    fifth homework was made

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Chats } from "./components/Chats";
 import { Profile } from "./components/Profile";
+import { Provider } from "react-redux";
+import { store } from "./store/index";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/chats" element={<Chats />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
-        <Route exact path="/" element={<HomePage />}></Route>
-        <Route exact path="/chats/:1" element={<Chats />}></Route>
-        <Route exact path="/chats/:2" element={<Chats />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chats" element={<Chats />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route exact path="/" element={<HomePage />}></Route>
+          <Route exact path="/chats/:1" element={<Chats />}></Route>
+          <Route exact path="/chats/:2" element={<Chats />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
+import { useSelector, useDispatch } from "react-redux";
+
 export const Profile = () => {
+  const { showName, name } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const EXAMPLE_ACTION = "EXAMPLE_ACTION";
+  const setShowName = () => {
+    dispatch({ type: EXAMPLE_ACTION });
+  };
   return (
     <div>
-      <h2>Profile</h2>
+      <h4>Profile</h4>
+      <input
+        type="checkbox"
+        checked={showName}
+        value={showName}
+        onChange={setShowName}
+      />
+      <span>Show Name</span>
+      {showName && <div>{name}</div>}
     </div>
   );
 };
